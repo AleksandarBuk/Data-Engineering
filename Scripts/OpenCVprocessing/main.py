0548bf8975c65518982703375712177cfaa84eca
AleksandarBuk <aleksandar.buk@gmail.com>
2023-09-16 18:15:01 +0200

    Updated JSON format

--- Scripts/OpenCVprocessing/main.py
+++ Scripts/OpenCVprocessing/main.py
@@ -1,13 +1,11 @@
 import cv2
 import json
-import os
 from tqdm import tqdm
 
-
-# Load and Extract Frames
-
-target_width = 120  # Set your desired width
-target_height = 120  # Set your desired height
+# Set your desired target resolution
+target_width = 120
+target_height = 120
+frames_per_second = 30  # Frames per second in the video
 
 def main(video_path, output_json_file):
     try:
@@ -18,16 +16,14 @@ def main(video_path, output_json_file):
             print("Error: Could not open video file.")
             return
 
-        frame_list = []  # List to store frame dictionaries
-
-        # Create the output directory if it doesn't exist
-        # if not os.path.exists(output_dir):
-        #     os.makedirs(output_dir)
-
         frame_number = 0
-        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # Get total number of frames
+        frame_list = []
 
-        # Progress bar
+        # Get the total number of frames and frames per second
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+
+        # Create a progress bar
         progress_bar = tqdm(total=total_frames, desc="Processing Frames", unit="frames")
 
         while True:
@@ -36,32 +32,23 @@ def main(video_path, output_json_file):
             if not ret:
                 break  # Reached the end of the video
 
+            # Resize the frame to the target resolution
             frame = cv2.resize(frame, (target_width, target_height))
 
+            # Calculate the frame timestamp in milliseconds
+            timestamp_ms = (frame_number * 1000) / fps
 
-            # Get the timestamp of the frame (in milliseconds)
-            timestamp_ms = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+            # Calculate the second in which the frame appears
+            frame_second = timestamp_ms / 1000.0
 
-            # Convert the frame to a list of pixel values
-            frame_data = frame.tolist()
-
-            # Create a dictionary to store frame information
+            # Create a dictionary to represent the frame
             frame_info = {
                 "frame_number": frame_number,
-                "timestamp_ms": timestamp_ms,
-                "frame_data": frame_data
+                "second": frame_second,
+                "timestamp_ms": timestamp_ms
             }
 
             frame_list.append(frame_info)
-
-            # Save the frame data as a JSON file
-            # frame_json_path = os.path.join(output_dir, f"frame_{frame_number}.json")
-            try:
-                with open(output_json_file, "w") as json_file:
-                    json.dump(frame_list, json_file, indent=4)
-
-            except IOError as e:
-                print(f"Error saving frame {frame_number} as JSON: {e}")
 
             frame_number += 1
             progress_bar.update(1)  # Update Progress Bar
@@ -71,13 +58,18 @@ def main(video_path, output_json_file):
 
         # Release the video capture object and close the video file
         cap.release()
-        cv2.destroyAllWindows()
 
-        print(f"Frames saved as JSON files in {output_json_file}")
+        # Create a JSON object with "frames" containing the frame list
+        json_data = {"frames": frame_list}
+
+        # Save the JSON data to the output file
+        with open(output_json_file, "w") as json_file:
+            json.dump(json_data, json_file, indent=4)
+
+        print(f"Frames saved as JSON file: {output_json_file}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
-
 
 if __name__ == "__main__":
     video_path = "./mp4/test.mp4"  # Replace with your video file path
